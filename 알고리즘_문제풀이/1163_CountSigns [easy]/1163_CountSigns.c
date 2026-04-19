e041f12d35780a128b84cf04b33bec3e5bf9e2ac
Boardwheelet <79092794+Boardwheelet@users.noreply.github.com>
2026-04-19 22:21:03 +0900

    음수, 양수 개수 로직 구현 / README 추가

---
+++ 알고리즘_문제풀이/1163_CountSigns [easy]/1163_CountSigns.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+
+	for (int i = 0; i < testcase; i++) {
+		int sign = 0, unsign = 0; // 음수, 양수
+
+		int n;
+		scanf("%d", &n);
+
+		for (int j = 0; j < n; j++) {
+			int input;
+			scanf("%d", &input);
+
+			if (input > 0) { unsign++; }
+			else if (input < 0) { sign++; }
+		}
+
+		printf("%d %d\n", sign, unsign);
+	}
+
+
+	return 0;
+}

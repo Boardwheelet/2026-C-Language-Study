52ef6b5a3503499333601dcf9baea7b3ff834d3d
Boardwheelet <79092794+Boardwheelet@users.noreply.github.com>
2026-04-19 21:26:35 +0900

    역삼각형(알파벳)| 그리기 로직 및 md파일

---
+++ 알고리즘_문제풀이/1173_Star(inverted triangle)/1173_Star(inverted triangle).c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+	int test_case;
+	scanf("%d", &test_case);
+
+	for (int i = 0; i < test_case; i++) {
+		int size;
+		scanf("%d", &size);
+
+		int alph = 0;
+		for (int j = size; j > 0; j--) { // 가로 길이 갯수
+			for (int k = 0; k < j; k++) {
+				printf("%c", 65 + alph);
+			}
+
+			alph++;
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}

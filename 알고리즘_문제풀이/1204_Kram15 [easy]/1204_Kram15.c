0ef1b02bdca9c5f709c25e44afd2ce9c25018565
Boardwheelet <79092794+Boardwheelet@users.noreply.github.com>
2026-04-19 21:50:29 +0900

    kram15 구매 대작전 로직 구현 및 readme

---
+++ 알고리즘_문제풀이/1204_Kram15 [easy]/1204_Kram15.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+
+	for (int i = 0; i < testcase; i++) {
+		int price;
+		scanf("%d", &price);
+
+		int sum = 0;
+		int count = 0;
+		for (int j = 1; sum < price; j++) {
+			if (j % 7 == 6 || j % 7 == 0) { // 주말이벤트
+				sum += 80000;
+			}
+			else { // 평일
+				sum += 40000;
+			}
+			count++;
+		}
+
+		printf("%d\n", count);
+	}
+
+
+	return 0;
+}

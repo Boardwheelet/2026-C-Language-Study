9b7290c62666cf0dc9b610b086007f507ba9d7fe
Boardwheelet <79092794+Boardwheelet@users.noreply.github.com>
2026-04-19 22:09:20 +0900

    Kram 17 구매 대작전 로직 구현 / README 작성

---
+++ 알고리즘_문제풀이/1087_Kram17 [easy]/1087_Kram17.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+
+	for (int i = 0; i < testcase; i++) {
+		int price;
+		scanf("%d", &price);
+
+		int sum = 0;
+		int count = 0;
+		for (int j = 1; sum < price; j++) {
+			if (j % 7 == 5 || j % 7 == 6 || j % 7 == 0) { // 주말이벤트
+				sum += 60000;
+			}
+			else { // 월 ~ 목요일
+				sum += 36000;
+			}
+			count++;
+		}
+
+		printf("%d\n", count);
+	}
+
+
+	return 0;
+}
